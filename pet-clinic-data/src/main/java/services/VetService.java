a3851b55049571e23ad2ab00aef19a89a9a465be
mariaBarczyk <barczyk.maria92@gmail.com>
2018-09-06 16:37:23 +0200

    Create interfaces for services

---
+++ pet-clinic-data/src/main/java/services/VetService.java
@@ -0,0 +1,14 @@
+package services;
+
+import com.springcourse.petclinic.models.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
