996bff05a13b9994d767df549a63dce65d0abbb6
mariaBarczyk <barczyk.maria92@gmail.com>
2018-09-07 11:25:36 +0200

    Create common crud interface for services

--- pet-clinic-data/src/main/java/com/springcourse/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/springcourse/petclinic/services/PetService.java
@@ -2,13 +2,6 @@ package com.springcourse.petclinic.services;
 
 import com.springcourse.petclinic.models.Pet;
 
-import java.util.Set;
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
