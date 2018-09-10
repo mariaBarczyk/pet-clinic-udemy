a8cd9671faf89c459f831c5d8434dc04aafa7bb2
mariaBarczyk <barczyk.maria92@gmail.com>
2018-09-10 10:37:18 +0200

    Create controllers for homepage, owners and vets

---
+++ pet-clinic-web/src/main/java/com/springcourse/petclinic/controllers/OwnerController.java
@@ -0,0 +1,13 @@
+package com.springcourse.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "owners", "owners.html"})
+    public String getOwners() {
+        return "owners/index.html";
+    }
+}
