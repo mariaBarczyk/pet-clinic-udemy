b7ca42902c6f2de6b28f70aebbd0910963167aff
mariaBarczyk <barczyk.maria92@gmail.com>
2018-09-20 09:08:34 +0200

    Create speciality map service

---
+++ pet-clinic-data/src/main/java/com/springcourse/petclinic/services/map/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package com.springcourse.petclinic.services.map;
+
+import com.springcourse.petclinic.models.Speciality;
+import com.springcourse.petclinic.services.CrudService;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long>  {
+}
