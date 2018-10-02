f89c6154bce8898bc8e3d561e6d085b2a5721046
mariaBarczyk <barczyk.maria92@gmail.com>
2018-10-02 20:17:19 +0200

    Add visit data loading on startup

--- pet-clinic-web/src/main/java/com/springcourse/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springcourse/petclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package com.springcourse.petclinic.bootstrap;
 
 import com.springcourse.petclinic.models.*;
-import com.springcourse.petclinic.services.OwnerService;
-import com.springcourse.petclinic.services.PetTypeService;
-import com.springcourse.petclinic.services.SpecialityService;
-import com.springcourse.petclinic.services.VetService;
+import com.springcourse.petclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +14,14 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialityService specialityService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialityService = specialityService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -88,6 +87,13 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(kateCat);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(kateCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+
+        visitService.save(catVisit);
 
         System.out.println("owners loaded");
 
