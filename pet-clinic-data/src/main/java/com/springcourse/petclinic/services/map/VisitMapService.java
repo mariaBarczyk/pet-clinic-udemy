7e4a3b64b210bdac124cfc2b87b25ea6e16bb10d
mariaBarczyk <barczyk.maria92@gmail.com>
2018-10-02 19:29:40 +0200

    Implement visit map service

---
+++ pet-clinic-data/src/main/java/com/springcourse/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,35 @@
+package com.springcourse.petclinic.services.map;
+
+import com.springcourse.petclinic.models.Visit;
+import com.springcourse.petclinic.repositories.VisitRepository;
+import com.springcourse.petclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
