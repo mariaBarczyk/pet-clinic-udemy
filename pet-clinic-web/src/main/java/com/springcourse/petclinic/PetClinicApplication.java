6588128854ecaf839e6f27318adb154ea4a377fe
mariaBarczyk <barczyk.maria92@gmail.com>
2018-10-02 15:47:04 +0200

    Add componetnt  scanning annotation to application

--- pet-clinic-web/src/main/java/com/springcourse/petclinic/PetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/springcourse/petclinic/PetClinicApplication.java
@@ -2,8 +2,10 @@ package com.springcourse.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class PetClinicApplication {
 
     public static void main(String[] args) {
