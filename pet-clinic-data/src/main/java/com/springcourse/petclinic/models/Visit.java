efcbd1710397ac09bc55bf91efa0d52a77354b8a
mariaBarczyk <barczyk.maria92@gmail.com>
2018-10-02 17:40:22 +0200

    Add jpa annotations to Visit model

--- pet-clinic-data/src/main/java/com/springcourse/petclinic/models/Visit.java
+++ pet-clinic-data/src/main/java/com/springcourse/petclinic/models/Visit.java
@@ -1,11 +1,18 @@
 package com.springcourse.petclinic.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import java.time.LocalDate;
 
+@Entity
 public class Visit extends BaseEntity {
 
     private LocalDate date;
     private String description;
+
+    @ManyToOne
+    @JoinColumn
     private Pet pet;
 
     public LocalDate getDate() {
