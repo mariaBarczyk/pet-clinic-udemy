d307cdc087f87697eac5f29d46dc6f660d8e796d
mariaBarczyk <barczyk.maria92@gmail.com>
2018-10-03 22:30:09 +0200

    Update to Junit5

--- pet-clinic-web/src/test/java/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/PetClinicApplicationTests.java
@@ -1,14 +1,15 @@
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class PetClinicApplicationTests {
-//
-//    @Test
-//    public void contextLoads() {
-//    }
-//
-//}
+import com.springcourse.petclinic.PetClinicApplication;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = PetClinicApplication.class)
+public class PetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
